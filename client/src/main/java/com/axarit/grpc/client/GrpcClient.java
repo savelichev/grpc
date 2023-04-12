cb6be15a1a559cdbfd93b92e9781c0738f09c153
Savelichev <mykola.s@miro.com>
2023-04-12 23:09:27 +0300

    sync and async unary calls

--- client/src/main/java/com/axarit/grpc/client/GrpcClient.java
+++ client/src/main/java/com/axarit/grpc/client/GrpcClient.java
@@ -5,6 +5,7 @@ import com.axarit.grpc.HelloResponse;
 import com.axarit.grpc.HelloServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 public class GrpcClient {
 
@@ -14,15 +15,54 @@ public class GrpcClient {
                 .usePlaintext()
                 .build();
 
+        asyncStub(channel);
+        blockingStub(channel);
+
+        channel.shutdown();
+
+    }
+
+    private static void asyncStub(ManagedChannel channel) {
+        final HelloServiceGrpc.HelloServiceStub stub = HelloServiceGrpc.newStub(channel);
+        final HelloRequest request = HelloRequest.newBuilder()
+                .setFirstName("Axarit")
+                .setLastName("gRPC - asyncStub")
+                .build();
+
+        stub.hello(
+                request,
+                new StreamObserver<>() { //Callback
+                    @Override
+                    public void onNext(HelloResponse value) {
+                        System.out.println(value.getGreeting());
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("axa onCompleted");
+                    }
+                }
+        );
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void blockingStub(ManagedChannel channel) {
         HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
 
         HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
                 .setFirstName("Axarit")
-                .setLastName("gRPC")
+                .setLastName("gRPC - blockingStub")
                 .build());
 
         System.out.println(helloResponse.getGreeting());
-
-        channel.shutdown();
     }
 }
